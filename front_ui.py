5ea8289f6d9939f3ce1d916b3f841cb415770cf7
Rodrigo Bruns <rodrigobrunsfh@gmail.com>
2023-09-15 12:46:41 -0300

    inital front

---
+++ front_ui.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'front.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(1107, 753)
+        self.openFileButton = QPushButton(Form)
+        self.openFileButton.setObjectName(u"openFileButton")
+        self.openFileButton.setGeometry(QRect(70, 500, 171, 81))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.openFileButton.setText(QCoreApplication.translate("Form", u"PushButton", None))
+    # retranslateUi
+
