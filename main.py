afc1c626e8b14fd0d3388c1781fe2d9d9da00840
Rodrigo Bruns <rodrigobrunsfh@gmail.com>
2023-09-18 10:58:16 -0300

    finalizado o separador

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 import os
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
@@ -7,13 +6,13 @@ import sep  # Importe o módulo sep
 
 
 class App(QMainWindow):
-    def __init__(self):
+    def __init__(self, app):
         super().__init__()
         self.ui = Ui_form()
         self.ui.setupUi(self)
         self.ui.openFileButton.clicked.connect(self.show_file_dialog)
-        self.ui.separateFileButton.clicked.connect(
-            self.separate_file)  # Novo botão para separação
+        self.ui.separateFileButton.clicked.connect(self.separate_file)
+        self.app = app
 
         # Variável de controle para rastrear se um arquivo foi selecionado
         self.file_selected = False
@@ -26,7 +25,7 @@ class App(QMainWindow):
         if file_dialog.exec():
             selected_files = file_dialog.selectedFiles()
             if selected_files:
-                self.file_selected = True  # Marque como True quando um arquivo for selecionado
+                self.file_selected = True
 
                 file_names = ", ".join(os.path.basename(file)
                                        for file in selected_files)
@@ -34,24 +33,20 @@ class App(QMainWindow):
                 current_directory = os.getcwd()
                 upload_folder = os.path.join(current_directory, "upload_file")
 
-                # Crie a pasta 'upload_file' se não existir
                 if not os.path.exists(upload_folder):
                     os.makedirs(upload_folder)
 
-                # Verifique se a pasta já possui arquivos
                 existing_files = os.listdir(upload_folder)
                 if existing_files:
-                    # Pasta contém arquivos, solicite confirmação do usuário
                     msg_box = QMessageBox()
                     msg_box.setIcon(QMessageBox.Question)
                     msg_box.setText(
                         "A pasta 'upload_file' já contém arquivos. Deseja apagá-los e adicionar os novos arquivos?")
                     msg_box.setStandardButtons(
                         QMessageBox.Yes | QMessageBox.No)
-                    result = msg_box.exec()
+                    result = msg_box.exec_()
 
                     if result == QMessageBox.Yes:
-                        # O usuário concordou em apagar os arquivos existentes
                         for existing_file in existing_files:
                             existing_file_path = os.path.join(
                                 upload_folder, existing_file)
@@ -67,7 +62,6 @@ class App(QMainWindow):
 
                 file_count = len(os.listdir(upload_folder))
 
-                # Atualizar a etiqueta na janela principal
                 if file_count == 1:
                     self.ui.file_info_label.setText(
                         f"{file_names}")
@@ -81,13 +75,12 @@ class App(QMainWindow):
                                 "Nenhum arquivo foi selecionado.")
             return
 
-        # Chame a função de separação do módulo sep
         sep.separate_sheet()
 
 
 def main():
     app = QApplication(sys.argv)
-    window = App()
+    window = App(app)
     window.show()
     sys.exit(app.exec_())
 
--- sep.py
+++ sep.py
@@ -1,21 +1,115 @@
-# sep.py
 import pandas as pd
 import os
 import glob
-from PySide6.QtWidgets import QMessageBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QRadioButton, QButtonGroup, QMessageBox, QInputDialog, QWidget, QTextEdit
+from PySide6.QtCore import Qt
+import sys
+
+
+class OutputWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Saída da Divisão")
+        self.layout = QVBoxLayout()
+
+        self.text_output = QTextEdit()
+        self.text_output.setReadOnly(True)
+        self.layout.addWidget(self.text_output)
+
+        self.close_button = QPushButton("Fechar")
+        self.close_button.clicked.connect(self.close)
+        self.layout.addWidget(self.close_button)
+
+        self.setLayout(self.layout)
+
+    def append_text(self, text):
+        self.text_output.append(text)
+
+    def show_confirmation(self, message):
+        confirmation = QMessageBox.question(
+            self, "Continuar?", message, QMessageBox.Yes | QMessageBox.No)
+        if confirmation == QMessageBox.Yes:
+            return True
+
+
+def split_base(output_name, choice, part_size, num_parts, remainder, planilha, output_window):
+    output_window.append_text(
+        f'Haverá {num_parts} planilhas com {part_size} linhas cada')
+    if remainder:
+        output_window.append_text(f'Mais uma com {remainder} linhas')
+
+    # Verificação
+    message = "Deseja continuar?"
+    if not output_window.show_confirmation(message):
+        return
+
+    # Dividir o DataFrame em partes
+    parts = [planilha[i:i + part_size]
+             for i in range(0, planilha.shape[0], part_size)]
+
+    # Salvar as partes em arquivos separados
+    for i, part in enumerate(parts):
+        file_name = f'output_file/{output_name} parte {i + 1} {part.shape[0]}l.xlsx'
+        part.to_excel(file_name, index=False)
+        output_window.append_text(f'Parte {i + 1} salva como {file_name}.')
+
+
+class SeparateSheetDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Opções de Separação")
+        self.layout = QVBoxLayout()
+
+        self.radio_group = QButtonGroup()
+
+        self.radio_lines = QRadioButton("Número de Linhas")
+        self.radio_lines.setChecked(True)
+        self.radio_columns = QRadioButton("Número de Planilhas")
+
+        self.radio_group.addButton(self.radio_lines, 1)
+        self.radio_group.addButton(self.radio_columns, 2)
+
+        self.layout.addWidget(self.radio_lines)
+        self.layout.addWidget(self.radio_columns)
+
+        self.split_button = QPushButton("Separar")
+        self.split_button.clicked.connect(self.on_split_button_clicked)
+        self.layout.addWidget(self.split_button)
+
+        self.back_button = QPushButton("Voltar")
+        self.back_button.clicked.connect(
+            self.reject)  # Fecha a janela de diálogo
+        self.layout.addWidget(self.back_button)
+
+        self.setLayout(self.layout)
+
+    def on_split_button_clicked(self):
+        choice = self.radio_group.checkedId()
+        if choice == 1:  # Número de Linhas
+            part_size, ok = QInputDialog.getInt(
+                self, "Número de Linhas", "Digite o número de linhas em cada arquivo:")
+            if ok:
+                self.part_size = part_size
+                self.accept()  # Fecha a janela de diálogo e prossegue com a separação
+        elif choice == 2:  # Número de Planilhas
+            num_parts, ok = QInputDialog.getInt(
+                self, "Número de Planilhas", "Digite o número de partes desejadas:")
+            if ok:
+                self.num_parts = num_parts
+                self.accept()  # Fecha a janela de diálogo e prossegue com a separação
 
 
 def separate_sheet():
-    global return_menu
     output_name = ""
-    return_menu = False  # Variável para voltar para o menu
+
     # Verifique se há pelo menos um arquivo .xlsx
     xlsx_files = glob.glob("upload_file/*.xlsx")
 
     if not xlsx_files:
         QMessageBox.warning(
-            None, "Nenhum Arquivo", "Nenhum arquivo .xlsx encontrado na pasta 'upload_file'. Por favor, adicione um arquivo .xlsx antes de continuar.", QMessageBox.Ok)
-        return_menu = True  # Defina para True para voltar para o menu
+            None, "Nenhum Arquivo", "Nenhum arquivo .xlsx encontrado na pasta 'upload_file'. Por favor, adicione um arquivo .xlsx, ou converta antes de continuar.", QMessageBox.Ok)
         return
 
     base_file = xlsx_files[0]
@@ -23,70 +117,33 @@ def separate_sheet():
 
     planilha = pd.read_excel(base_file)
 
-    # Método de separação
-    total_rows = planilha.shape[0]
-    print(f'A base {base_file_name} contém {total_rows} linhas.')
-    print('Deseja dividir por:')
-    print('1 - Número de linhas por parte')
-    print('2 - Número de planilhas')
-    print('3 - Voltar para o menú')
-    # choice = int(input())
-    # Define o número máximo de linhas em cada parte
-    if not return_menu:
-        output_name = input('Digite o nome do arquivo de saída: ')
+    dialog = SeparateSheetDialog()
+    result = dialog.exec_()
 
-    def split_base(output_name):
-        print(f'Haverá {num_parts} planilhas com {part_size} linhas cada')
-        if remainder:
-            print(f'Mais uma com {remainder} linhas')
+    if result == QDialog.Accepted:
+        choice = dialog.radio_group.checkedId()
 
-        # Verificação
-        continue_prompt = input('Deseja continuar? Y/n')
-        if continue_prompt.lower() == 'n':
-            exit()
+        if choice == 1:  # Número de Linhas
+            part_size = dialog.part_size
+            num_parts = planilha.shape[0] // part_size
+            remainder = planilha.shape[0] % part_size
+        elif choice == 2:  # Número de Planilhas
+            num_parts = dialog.num_parts
+            part_size = planilha.shape[0] // num_parts
+            remainder = planilha.shape[0] % num_parts
 
-        # Dividir o DataFrame em partes
-        parts = [planilha[i:i + part_size]
-                 for i in range(0, planilha.shape[0], part_size)]
+        output_window = OutputWindow()
+        output_window.show()
+        split_base(output_name, choice, part_size,
+                   num_parts, remainder, planilha, output_window)
 
-        # Salvar as partes em arquivos separados
-        for i, part in enumerate(parts):
-            file_name = f'output_file/{output_name} parte {i + 1} {part.shape[0]}l.xlsx'
-            part.to_excel(file_name, index=False)
-            print(f'Parte {i + 1} salva como {file_name}.')
+        QMessageBox.information(
+            None, "Concluído", "Processo de divisão concluído.", QMessageBox.Ok)
 
-    if choice == 1:
-        part_size = int(
-            input('Digite o número de linhas em cada arquivo: '))
 
-        # Verificando se há linhas suficientes para dividir
-        if part_size > total_rows:
-            print(
-                'O número de linhas em cada parte é maior do que o número de linhas na planilha.')
-            exit()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-        num_parts = total_rows // part_size
-        remainder = total_rows % part_size
+    separate_sheet()
 
-        split_base(output_name)
-
-    elif choice == 2:
-        num_parts = int(input('Digite o número de partes desejadas: '))
-
-        # Verificando se há linhas suficientes para dividir
-        if num_parts > total_rows:
-            print('O número de partes é maior do que o número de linhas na planilha.')
-            exit()
-
-        # Calculando o número de linhas em cada parte
-        part_size = total_rows // num_parts
-        remainder = total_rows % num_parts
-        split_base(output_name)
-
-    elif choice == 3:
-        return_menu = True
-        print('Voltando ')
-        return return_menu
-
-    else:
-        print('Opção inválida')
+    sys.exit(app.exec_())
